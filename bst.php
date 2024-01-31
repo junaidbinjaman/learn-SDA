3a8d23afbd1858cde54bd26db9afa46804eb378d
Junaid Bin Jaman <junaidbinjaman@Junaids-MacBook-Pro.local>
2024-01-31 16:13:34 +0600

    Trees are completed

--- bst.php
+++ bst.php
@@ -32,7 +32,7 @@ class Node {
 	}
 }
 
-class BinarySearchClass {
+class BinarySearchTree {
 	public $root;
 
 	public function __construct() {
@@ -40,28 +40,28 @@ class BinarySearchClass {
 	}
 
 	public function insert( $data ) {
-		$this->insertNode( $this->root, $data );
+		$this->root = $this->insertNode( $this->root, $data );
 	}
 
 	private function insertNode( $node, $data ) {
 		if ( $node === null ) {
 			$node = new Node( $data );
-			return;
+			return $node;
 		}
 
 		if ( $data < $node->data ) {
 			$node->left = $this->insertNode( $node->left, $data );
-			return;
 		}
 
 		if ( $data > $node->data ) {
 			$node->right = $this->insertNode( $node->right, $data );
-			return;
 		}
+
+		return $node;
 	}
 
 	public function search( $data ) {
-		$this->searchNode( $this->root, $data );
+		return $this->searchNode( $this->root, $data );
 	}
 
 	private function searchNode( $node, $data ) {
@@ -78,17 +78,70 @@ class BinarySearchClass {
 		}
 	}
 
-    public function delete($data) {
-        $this->root = $this->deleteNode($this->root, $data);
-    }
-    
-    private function deleteNode($node, $data) {
-        // Implementation for deleting a node
-    }    
+	public function delete( $data ) {
+		$this->root = $this->deleteNode( $this->root, $data );
+	}
 
+	private function deleteNode( $node, $data ) {
+		if ( $node === null ) {
+			return $node;
+		}
+
+		if ( $data < $node->data ) {
+			$node->left = $this->deleteNode( $node->left, $data );
+		}
+
+		if ( $data > $node->data ) {
+			$node->right = $this->deleteNode( $node->right, $data );
+		}
+
+		if ( $data === $node->data ) {
+			if ( $node->left === null ) {
+				return $node->right;
+			}
+
+			if ( $node->right === null ) {
+				return $node->left;
+			}
+
+			$node->data  = $this->minValueNode( $node->right )->data;
+			$node->right = $this->deleteNode( $node->right, $node->data );
+		}
+
+		return $node;
+	}
+
+	private function minValueNode( $node ) {
+		while ( $node->left !== null ) {
+			$node = $node->left;
+		}
+		return $node;
+	}
+
+	public function traverseInOrder() {
+		$this->inOrder( $this->root );
+	}
+
+	private function inOrder( $node ) {
+		if ( $node === null ) {
+			return;
+		}
+
+		$this->inOrder( $node->left );
+
+		echo $node->data . ' ';
+
+		$this->inOrder( $node->right );
+	}
 }
 
 $bst = new BinarySearchTree();
 $bst->insert( 15 );
 $bst->insert( 10 );
 $bst->insert( 20 );
+$bst->insert( 200 );
+
+$bst->delete(200);
+
+echo 'In-order traversal: ';
+$bst->traverseInOrder();
